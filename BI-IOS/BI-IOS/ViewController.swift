45228891666c2e4245ff2c72a2674abe205c0e38
Jan Misar <misar.jan@gmail.com>
2017-10-17 10:46:08 +0200

    View using NSLayoutConstraints in code.

--- BI-IOS/BI-IOS/ViewController.swift
+++ BI-IOS/BI-IOS/ViewController.swift
@@ -18,6 +18,18 @@ class ViewController: UIViewController {
         
         // self.label is defined in Main.storyboard
         
+        // Layout definition using basic NSLayoutConstraints
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        view.addSubview(greenView)
+        
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        let leading = NSLayoutConstraint(item: greenView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 10)
+        let trailing = NSLayoutConstraint(item: greenView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: -10)
+        let top = NSLayoutConstraint(item: greenView, attribute: .top, relatedBy: .equal, toItem: label, attribute: .bottom, multiplier: 1, constant: 10)
+        let height = NSLayoutConstraint(item: greenView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraints([leading, trailing, top])
+        greenView.addConstraint(height)
     }
     
     override func viewDidLoad() {
