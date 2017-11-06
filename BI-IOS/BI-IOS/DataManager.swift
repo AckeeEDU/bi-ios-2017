9fc3a013a88d09cf77806c50c36d67ee2d9a9d2d
Dominik Vesely <dominik@Dominiks-MacBook-Pro.local>
2017-11-06 21:43:41 +0100

    simple download

--- BI-IOS/BI-IOS/DataManager.swift
+++ BI-IOS/BI-IOS/DataManager.swift
@@ -13,17 +13,13 @@ class DataManager {
     
     func getReipes( callback: ((Any) -> ())?)  {
         Alamofire.request("https://cookbook.ack.ee/api/v1/recipes").responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-            
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-                callback?(json)
-            }
-            
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
+         
+            if let data = response.data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let recipes = try jsonDecoder.decode([Recipe].self, from: data)
+                    print(recipes)
+                } catch {}
             }
         }
     }
---
+++ BI-IOS/BI-IOS/Model/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  BI-IOS
+//
+//  Created by Dominik Vesely on 06/11/2017.
+//  Copyright © 2017 ČVUT. All rights reserved.
+//
+
+import Foundation
+
+// {"name":"Ackee and Saltfish","duration":10,"id":"5a00be7f0898a11000146b64","score":0}
+struct Recipe : Codable {
+    var name : String
+    var duration : Int
+    var id : String
+    var score : Int
+}
