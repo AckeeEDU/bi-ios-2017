124825ca2979eb07d549932552e8fd0515fec6b0
Jan Misar <misar.jan@gmail.com>
2017-11-21 08:55:41 +0100

    Fetch locations from DB.

--- BI-IOS/BI-IOS/MapViewController.swift
+++ BI-IOS/BI-IOS/MapViewController.swift
@@ -41,11 +41,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
         mapView.removeAnnotations(mapView.annotations)
         
-        locations.forEach { locationDict in
+        let locations = FavoriteLocation.mr_findAll() as! [FavoriteLocation]
+        
+        locations.forEach { location in
             let annotation = MKPointAnnotation()
-            annotation.coordinate = CLLocationCoordinate2D(latitude: locationDict["lat"]!, longitude: locationDict["lon"]!)
-            annotation.title = "A location"
-            annotation.subtitle = "A location subtitle"
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude!.doubleValue, longitude: location.longitude!.doubleValue)
+            annotation.title = location.title
             mapView.addAnnotation(annotation)
         }
         
@@ -119,7 +120,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         let button = UIButton(type: .detailDisclosure)
         annotationView.rightCalloutAccessoryView = button
         
-        annotationView.detailCalloutAccessoryView = UIImageView(image: #imageLiteral(resourceName: "pin"))
+        //annotationView.detailCalloutAccessoryView = UIImageView(image: #imageLiteral(resourceName: "pin"))
         
         annotationView.isDraggable = true // that's nonsense here of course 😏 - just for example
         
@@ -151,28 +152,28 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
     }
     
-    let locations = [
-        ["lat": 50.10155117, "lon": 14.50131164],
-        ["lat": 50.04845155, "lon": 14.40643163],
-        ["lat": 50.01436603, "lon": 14.48202576],
-        ["lat": 50.09773545, "lon": 14.42862526],
-        ["lat": 50.02386574, "lon": 14.418157],
-        ["lat": 50.10891206, "lon": 14.50023615],
-        ["lat": 50.13137991, "lon": 14.43188124],
-        ["lat": 50.03085971, "lon": 14.43437316],
-        ["lat": 50.05523586, "lon": 14.36531867],
-        ["lat": 50.12467219, "lon": 14.39459484],
-        ["lat": 50.00616185, "lon": 14.41959398],
-        ["lat": 50.06693629, "lon": 14.43925259],
-        ["lat": 50.08936261, "lon": 14.53516745],
-        ["lat": 50.03396537, "lon": 14.48803967],
-        ["lat": 50.06252636, "lon": 14.49942098],
-        ["lat": 50.01692711, "lon": 14.37874073],
-        ["lat": 50.07238541, "lon": 14.37937722],
-        ["lat": 50.02807288, "lon": 14.51289626],
-        ["lat": 50.0276592, "lon": 14.48751812],
-        ["lat": 50.1340302, "lon": 14.45877785]
-    ]
+//    let locations = [
+//        ["lat": 50.10155117, "lon": 14.50131164],
+//        ["lat": 50.04845155, "lon": 14.40643163],
+//        ["lat": 50.01436603, "lon": 14.48202576],
+//        ["lat": 50.09773545, "lon": 14.42862526],
+//        ["lat": 50.02386574, "lon": 14.418157],
+//        ["lat": 50.10891206, "lon": 14.50023615],
+//        ["lat": 50.13137991, "lon": 14.43188124],
+//        ["lat": 50.03085971, "lon": 14.43437316],
+//        ["lat": 50.05523586, "lon": 14.36531867],
+//        ["lat": 50.12467219, "lon": 14.39459484],
+//        ["lat": 50.00616185, "lon": 14.41959398],
+//        ["lat": 50.06693629, "lon": 14.43925259],
+//        ["lat": 50.08936261, "lon": 14.53516745],
+//        ["lat": 50.03396537, "lon": 14.48803967],
+//        ["lat": 50.06252636, "lon": 14.49942098],
+//        ["lat": 50.01692711, "lon": 14.37874073],
+//        ["lat": 50.07238541, "lon": 14.37937722],
+//        ["lat": 50.02807288, "lon": 14.51289626],
+//        ["lat": 50.0276592, "lon": 14.48751812],
+//        ["lat": 50.1340302, "lon": 14.45877785]
+//    ]
     
 }
 
