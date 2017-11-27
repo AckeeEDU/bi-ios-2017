b7bbaa818b00be519c1e43d3be40c897c8fd2d7a
Jan Misar <misar.jan@gmail.com>
2017-11-27 22:19:37 +0100

    Mapping and saving objects from API 🎉

--- Unicorn/Unicorn/APIService.swift
+++ Unicorn/Unicorn/APIService.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import MagicalRecord
 
 typealias FailureBlock = (Error) -> ()
 
@@ -15,17 +16,33 @@ class APIService {
     
     static let shared = APIService() // is that 100% ok?
     
-    func languages(success: @escaping (Any) -> (), failure: @escaping FailureBlock) {
+    func languages(success: @escaping ([Language]) -> (), failure: @escaping FailureBlock) {
         Alamofire.request("https://www.whostolemyunicorn.com/api/languages.json") // hardcoded URL? 🤔
             .validate()
             .responseJSON { response in
                 switch response.result {
                 case .success(let value):
-                    success(value)
+                    
+                    guard let languagesArray = value as? [[AnyHashable: Any]] else {
+                        failure(NSError()) // NSError()..? definitely not good...
+                        return
+                    }
+                    
+                    var languages: [Language] = []
+                    MagicalRecord.save({ localContext in
+                        languages = Language.mr_import(from: languagesArray, in: localContext) as! [Language] // force cast because old objC API
+                    }, completion: { (saved, error) in
+                        if error == nil {
+                            let languagesOnDefaultContext = languages.map { $0.mr_(in: NSManagedObjectContext.mr_default())! }
+                            success(languagesOnDefaultContext)
+                        } else {
+                            failure(NSError()) // NSError()..? nope.. not good even here
+                        }
+                    })
+                    
                 case .failure(let error):
                     failure(error)
                 }
-                // isn't it completely useless..?
         }
     }
     
--- Unicorn/Unicorn/AppDelegate.swift
+++ Unicorn/Unicorn/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        // setup CoreData
+        MagicalRecord.setupAutoMigratingCoreDataStack() // automigrating? what is that..?
+        
         let vc = UINavigationController(rootViewController: LanguagesViewController())
         
         window = UIWindow(frame: UIScreen.main.bounds)
