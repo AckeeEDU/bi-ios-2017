6d199745816f8e1418fbce2b0fcf72438946fe56
Jan Misar <misar.jan@gmail.com>
2017-11-27 22:45:05 +0100

    Language detail controller prepared. Project file structure.

---
+++ Unicorn/Unicorn/View/Controllers/LanguageDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LanguageDetailViewController.swift
+//  Unicorn
+//
+//  Created by Jan Misar on 27.11.17.
+//  Copyright © 2017 Jan Misar. All rights reserved.
+//
+
+import UIKit
+
+class LanguageDetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
