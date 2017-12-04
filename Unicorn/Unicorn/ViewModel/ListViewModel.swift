7bf69d03f6d6ec8a52cb350453b1efbbf6f3c529
Dominik Vesely <dominik.vesely@ackee.cz>
2017-12-04 20:15:10 +0100

    basic List VM

---
+++ Unicorn/Unicorn/ViewModel/ListViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  ListViewModel.swift
+//  Unicorn
+//
+//  Created by Dominik Vesely on 04/12/2017.
+//  Copyright © 2017 Jan Misar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListViewModel {
+    
+    private var model : Language
+    
+    
+    var displayName : String  {
+        get {
+            if let name = model.name, let abbr = model.abbr {
+                return "\(name) (\(abbr))"
+            }
+            return ""
+        }
+    }
+    var flagImageURL : URL?
+    var seen = false
+    
+    init (model: Language) {
+        self.model = model
+        self.flagImageURL = URL(string: model.flag!)
+    }
+}
