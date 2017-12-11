d6cc42cfa8393dbf72623c40ae3c92eeb0d88b32
Jan Misar <misar.jan@gmail.com>
2017-12-11 16:51:34 +0100

    We are drawing 🎉

--- BI-IOS/BI-IOS/Shared canvas/CanvasView.swift
+++ BI-IOS/BI-IOS/Shared canvas/CanvasView.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class CanvasView: UIView {
 
+    var points: [CGPoint] = []
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        backgroundColor = .white
         
         let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panChanged(_:)))
         addGestureRecognizer(gestureRecognizer)
@@ -26,21 +31,37 @@ class CanvasView: UIView {
         case .began:
             break
         case .changed:
-            print(gestureRecognizer.location(in: self))
+            let point = gestureRecognizer.location(in: self)
+            points.append(point)
             break
         case .cancelled, .ended, .failed:
             break
         default:
             break
         }
+        setNeedsDisplay()
     }
     
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.setLineWidth(1)
+        context.setStrokeColor(UIColor.black.cgColor)
+        
+        context.beginPath()
+        
+        if let firstPoint = points.first {
+            context.move(to: firstPoint)
+        }
+        context.addLines(between: points)
+        
+        context.drawPath(using: .stroke)
     }
-    */
+    
 
 }
