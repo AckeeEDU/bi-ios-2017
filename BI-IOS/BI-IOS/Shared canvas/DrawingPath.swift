3750a8b3217c88b3130eb10aa658aba1e334ed8b
Jan Misar <misar.jan@gmail.com>
2017-12-11 17:59:46 +0100

    Path serialization.

--- BI-IOS/BI-IOS/Shared canvas/DrawingPath.swift
+++ BI-IOS/BI-IOS/Shared canvas/DrawingPath.swift
@@ -14,4 +14,23 @@ class DrawingPath {
     var color: UIColor = .black
     var lineWidth: CGFloat = 1
     
+    static func deserialize(from object: Any) -> DrawingPath {
+        let result = DrawingPath()
+        
+        // todo...
+        
+        return result
+    }
+    
+    func serialize() -> [String: Any] {
+        var result: [String: Any] = [:]
+        
+        result["points"] = points.map { point in
+            return ["x": point.x, "y": point.y]
+        }
+        //result["color"] = ...
+        //result["lineWidth"] = ...
+        
+        return result
+    }
 }
